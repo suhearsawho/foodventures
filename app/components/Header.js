dedbad0be50edfcdc00a74da6c48cce32507e5c3
Susan Su <susansu.software@gmail.com>
2019-05-27 23:17:55 -0700

    Simple header bar added to page

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import { FlightTakeoff } from '@material-ui/icons';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
+import { withStyles } from '@material-ui/core/styles';
 
-export default function Logo ( {} ) {
+const styles = theme => ({
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  title: {
+    marginLeft: theme.spacing(4)
+  },
+  right: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'flex-end',
+  }
+});
+
+function Header (props) {
+  const { classes } = props;
+
   return (
-    <AppBar position="relative">
-      <Toolbar>
+    <AppBar position="fixed">
+      <Toolbar className={classes.toolbar}>
         <FlightTakeoff />        
-        <Typography variant="h6" color="inherit" noWrap>
+        <Typography variant="h6" color="inherit" className="title" noWrap>
           Foodventures
         </Typography>
-        <Button href="#">
-          Sign Up
-        </Button>
+        <div className={classes.right}>
+          <Button href="#">
+            Sign Up
+          </Button>
+        </div>
       </Toolbar>
     </AppBar>
   )
 }
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
--- app/index.js
+++ app/index.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from './components/Header';
+import { createMuiTheme } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+import './index.css'
 
 const useStyles = makeStyles(theme => ({
-
+  '@global': {
+    body: {
+      margin: 0,
+      padding: 0,
+    },
+  }
 }));
 
-class App extends Component {
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#b5ffff',
+      main: '#81d3f9',
+      dark: '#4ba2c6',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#b2fab4',
+      main: '#81c784',
+      dark: '#519657',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    fontFamily: "'Work Sans', sans-serif",
+    fontSize: 14,
+    fontWeightLight: 300, // Work Sans
+    fontWeightRegular: 400, // Work Sans
+    fontWeightMedium: 700, // Roboto Condensed
+    fontFamilySecondary: "'Roboto Condensed', sans-serif",
+  },
+});
 
+class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-      </div>
+      <React.Fragment>
+        <ThemeProvider theme={ theme }>
+          <CssBaseline />
+          <Header />
+        </ThemeProvider>
+      </React.Fragment>
     );
   }
 }
